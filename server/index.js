25b0a39d0cf9ffa28f555f5b500573ea44a91006
warap0n <iiryonpai@gmail.com>
2023-10-30 23:34:51 +0900

    server側のsocket 実装

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
+
 const PORT = 5000;
 const messageRoute = require("./routes/message");
 const roomRoute = require("./routes/room");
 const mongoose = require("mongoose");
 require("dotenv").config();
+
+// モデルのインポート
+const Room = require("./models/Room");
+const Message = require("./models/Message");
 
 //データベース接続
 mongoose
@@ -16,11 +27,37 @@ mongoose
     console.log(err);
   });
 
+//socket
+io.on("connection", (socket) => {
+  console.log("A user connected.");
+
+  // チャットメッセージを受信
+  socket.on("chatMessage", async (data) => {
+    const { text, roomId, sender } = data;
+
+    // メッセージをデータベースに保存
+    const newMessage = new Message({ text, roomId, sender });
+    await newMessage.save();
+
+    // メッセージをルームの参加者に送信
+    socket.to(roomId).emit("chatMessage", newMessage);
+  });
+
+  // ルームに参加
+  socket.on("joinRoom", (roomId) => {
+    socket.join(roomId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected.");
+  });
+});
+
 //ミドルウェア
 app.use(express.json());
 app.use("/api/message", messageRoute);
 app.use("/api/room", roomRoute);
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log("server is running on port 5000");
 });
